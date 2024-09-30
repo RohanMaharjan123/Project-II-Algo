5a1b05bfed10ab8daebdf9795781f9ace3e0d026
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-09-30 16:35:30 +0545

    fine tuned algorithm and finished the work for the project

--- src/client/src/helpers/Colors.jsx
+++ src/client/src/helpers/Colors.jsx
@@ -1,7 +1,7 @@
 const Colors = {
-  default: '#2761e0',
-  primary: '#fbfcfe',
-  secondary: '#909090;'
+  default: '#2761e0', // vibrant blue color.
+  primary: '#fbfcfe', // A very light, almost white blue color.
+  secondary: '#909090;' // A neutral gray color.
 };
 
 export default Colors;
